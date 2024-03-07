a5b8a70be8c2af562bda7ad9fc6888b28a73dddc
Fkshaikh <shaikhfaisal54@gmail.com>
2024-03-07 11:27:12 +0530

    Navbar Created

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,7 +1,183 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import { Avatar, Button, useTheme } from "@mui/material";
 
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 const NavBar = () => {
-  return <div>Navbar</div>;
+  const theme = useTheme();
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  return (
+    <div>
+      <AppBar position="static" sx={{ backgroundColor: theme.palette.bg1 }}>
+        <Container maxWidth="xl">
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <AcUnitIcon
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  mr: 1,
+                  color: theme.palette.text1,
+                }}
+              />
+              <Typography
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  color: theme.palette.text1,
+                  textDecoration: "none",
+                }}
+              >
+                Queue Management System
+              </Typography>
+            </Box>
+
+            {/* Mobile Size */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: { xs: "space-between", md: "none" },
+                position: "absolute",
+              }}
+            >
+              <AcUnitIcon
+                sx={{
+                  display: { xs: "flex", md: "none" },
+                  mr: 1,
+                  color: theme.palette.text1,
+                }}
+              />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  color: theme.palette.text1,
+                  textDecoration: "none",
+                }}
+              >
+                QMS
+              </Typography>
+            </Box>
+
+            {/* Profile  */}
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: "flex",
+                gap: "10px",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "10px",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  sx={{
+                    color: theme.palette.text2,
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: "10px",
+                    "&:hover": {
+                      color: theme.palette.text1,
+                    },
+                  }}
+                >
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      "&:hover": {
+                        color: theme.palette.text1,
+                      },
+                    }}
+                  >
+                    Add Task
+                  </Typography>
+                </Button>
+                <Button
+                  color="secondary"
+                  sx={{
+                    color: theme.palette.text1,
+                    backgroundColor: theme.palette.secondary.main,
+                    borderRadius: "10px",
+                  }}
+                >
+                  <Typography variant="h6">Settings</Typography>
+                </Button>
+              </Box>
+
+              <Tooltip
+                title="Profile"
+                PopperProps={{
+                  style: { fontSize: theme.typography.h6.fontSize },
+                }}
+              >
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar>H</Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,7 +1,6 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  // Define your theme properties here
   palette: {
     primary: {
       main: "#009963",
@@ -9,14 +8,34 @@ const theme = createTheme({
     secondary: {
       main: "#F5F0E5",
     },
-    text1: "1C170D",
-    text2: "FFFFFF",
+    text1: "#1C170D",
+    text2: "#FFFFFF",
+    bg1: "#FFFFFF",
   },
   typography: {
     fontFamily: "Plus Jakarta Sans, sans-serif",
     fontOpticalSizing: "auto",
-    fontWeight: "700",
+    fontWeight: 700,
     fontStyle: "normal",
+    fontSize: "16px",
+    h1: {
+      fontSize: "2.5rem",
+    },
+    h2: {
+      fontSize: "2rem",
+    },
+    h3: {
+      fontSize: "1.5rem",
+    },
+    h4: {
+      fontSize: "1.25rem",
+    },
+    h5: {
+      fontSize: "1rem",
+    },
+    h6: {
+      fontSize: "0.7rem",
+    },
   },
 });
 
